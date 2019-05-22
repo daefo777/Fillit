512d1373f07b1333bb4ddd5d37918a8eae32edd5
Im  Daeho <idaeho@e1z3r6p5.42.us.org>
2019-05-21 22:29:51 -0700

    start of fillit

---
+++ fillit.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idaeho <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 22:05:43 by idaeho            #+#    #+#             */
+/*   Updated: 2019/05/21 22:28:04 by idaeho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FILLIT_H
+# define FT_FILLIT_H
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
---
+++ validcheck.c
@@ -0,0 +1,14 @@
+
+#include "fillit.h"
+
+int		validcheck(int fd) 
+
+int		main(int ac, char **av)
+{
+	char	*buf
+	if (ac == 2)
+	{
+		open (av[1], O_RDONLY);
+		validcheck(fd);
+		// assemble(??)
+
